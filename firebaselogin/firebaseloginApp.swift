2e81a2561ff73a09d711456d8a90986fb9c878e8
Madhu <raydriiger@gmail.com>
2020-12-22 10:56:00 +1300

    Initial Commit

---
+++ firebaselogin/firebaseloginApp.swift
@@ -0,0 +1,17 @@
+//
+//  firebaseloginApp.swift
+//  firebaselogin
+//
+//  Created by Madhu Kalyan Mattaparthi on 22/12/20.
+//
+
+import SwiftUI
+
+@main
+struct firebaseloginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
